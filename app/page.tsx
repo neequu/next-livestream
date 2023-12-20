24ff8038e7376df871a6c11d47472ca0b2d2e6c1
neequu <nist1701@gmail.com>
2023-12-20 11:52:33 +0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="px-2 md:px-2">hello</div>;
+  return (
+    <div
+      className="
+  px-2 md:px-2"
+    >
+      <Button>test</Button>
+    </div>
+  );
 }
