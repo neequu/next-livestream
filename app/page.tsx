ae22cfe3d8db4dd170b5f75e1138758f01a77973
neequu <nist1701@gmail.com>
2023-12-20 15:51:40 +0300

    chore: add prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import prisma from "@/lib/prisma";
+
+async function getPost() {
+  const users = await prisma.user.findMany();
+  return users;
+}
 
 export default async function Home() {
-  const pets = ["zxc"];
-  return <div>{JSON.stringify(pets)}</div>;
+  const users = await getPost();
+  return <div>{JSON.stringify(users)}</div>;
 }
