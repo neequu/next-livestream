1674f24940780c666eb82e6ec7dc003e9e6fbb85
neequu <nist1701@gmail.com>
2023-12-23 16:55:46 +0300

    styling: change position of sidebar

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -4,15 +4,13 @@ import TheHeader from '@/components/TheHeader'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex flex-1 flex-col'>
+    <>
       <TheHeader />
-      <main className=' flex flex-1 flex-col bg-gray-700/90'>
-        <div className='container mx-auto flex flex-1 flex-col'>
-          {children}
-          <Sidebar />
-        </div>
+      <main className='relative flex-1 bg-gray-900/40'>
+        <Sidebar />
+        {children}
       </main>
-    </div>
+    </>
   )
 }
 
--- components/sidebar/SideBarWrapper.tsx
+++ components/sidebar/SideBarWrapper.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const SideBarWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>
+  return (
+    <aside className='absolute bottom-0 left-0 top-0 flex h-full w-60 flex-col border-r border-gray-500/40 bg-gray-700/80'>
+      {children}
+    </aside>
+  )
 }
 
 export default SideBarWrapper
