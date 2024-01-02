d5480cabddea88e7da6fcda749b7a8f720dc3e7c
neequu <nist1701@gmail.com>
2024-01-02 17:41:58 +0300

    feat: media hook for sidebar

--- components/sidebar/SideBarWrapper.tsx
+++ components/sidebar/SideBarWrapper.tsx
@@ -1,10 +1,18 @@
 'use client'
 import { cn } from '@/lib/utils'
 import useSidebarStore from '@/stores/sidebarStore'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useMediaQuery } from 'usehooks-ts'
 
 const SidebarWrapper = ({ children }: { children: React.ReactNode }) => {
-  const { collapsed } = useSidebarStore((state) => state)
+  const { collapsed, onCollapse, onExpand } = useSidebarStore((state) => state)
+  const isMobile = useMediaQuery('(min-width: 640px)')
+
+  useEffect(() => {
+    if (isMobile) onExpand()
+    else onCollapse()
+  }, [isMobile, onCollapse, onExpand])
+
   return (
     <aside
       className={cn(
