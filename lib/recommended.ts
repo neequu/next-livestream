b6f2975b0d2d08c913031ac23c240e49cefd1f47
neequu <nist1701@gmail.com>
2024-01-10 21:01:59 +0300

    feat: add public routes

--- lib/recommended.ts
+++ lib/recommended.ts
@@ -2,11 +2,31 @@ import { getCurrentUser } from './auth'
 import prisma from '@/lib/prisma'
 
 export async function getRecommended() {
-  const users = await prisma.user.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-  })
+  let userId
 
-  return users
+  try {
+    const currentUser = await getCurrentUser()
+    userId = currentUser.id
+  } catch {
+    userId = null
+  }
+
+  if (userId) {
+    return prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      where: {
+        NOT: {
+          id: userId,
+        },
+      },
+    })
+  } else {
+    return prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  }
 }
